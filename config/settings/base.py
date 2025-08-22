0ae6448d5d5a5ac4936387c64312a9d9bdefc15a
Jiwoong0322 <woong4624@gmail.com>
2025-08-22 18:02:18 +0900

    íºbuild : 빌드 관련 파일 수정 및 삭제한 경우
    1) develop머지 후 config/urls.py, config/settings/base.py 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -27,7 +27,7 @@ load_dotenv(dotenv_path=env_path)
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
