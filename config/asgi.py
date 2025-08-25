2ee14f55ac7f962c67a0f1d956590cc37d8a2875
박준혁 <jun201908@naver.com>
2025-08-25 14:52:06 +0900

    feat: create JWTAuthmiddleware, add JWT auth logic to chat consumer

--- config/asgi.py
+++ config/asgi.py
@@ -1,10 +1,10 @@
 import os
 
 import django
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from apps.common.middlewares import JWTAuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()  # 이 부분이 핵심입니다.
@@ -18,8 +18,6 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(apps.chats.routing.websocket_urlpatterns))
-        ),
+        "websocket": JWTAuthMiddlewareStack(URLRouter(apps.chats.routing.websocket_urlpatterns)),
     }
 )
