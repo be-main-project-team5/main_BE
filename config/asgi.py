cb2c03f9092373f2ca1855e8af8e445cf9f98be3
박준혁 <jun201908@naver.com>
2025-08-25 15:01:40 +0900

    chore: modify import order

--- config/asgi.py
+++ config/asgi.py
@@ -1,23 +1,19 @@
 import os
 
 import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
-from apps.common.middlewares import JWTAuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()  # 이 부분이 핵심입니다.
 
 # Django 설정이 로드된 후에 라우팅을 임포트합니다.
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 import apps.chats.routing
-
-django_asgi_app = get_asgi_application()
-
+from apps.common.middlewares import JWTAuthMiddlewareStack
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
+        "http": get_asgi_application(),
         "websocket": JWTAuthMiddlewareStack(URLRouter(apps.chats.routing.websocket_urlpatterns)),
     }
 )
